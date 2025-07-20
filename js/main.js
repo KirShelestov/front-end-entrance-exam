be65a231e71c3dddcf30a51655616927a80dad88
Kirill <k.shelestov@g.nsu.ru>
2025-07-20 18:47:22 +0700

    Fix style

--- js/main.js
+++ js/main.js
@@ -23,9 +23,44 @@ function syncToolsExperienceHeight() {
     }
 }
 
+function syncProfileBlocksHeight() {
+    const imgBlock = document.querySelector(".profile-image img");
+    const infoBlock = document.querySelector(".profile-info");
+    const langBlock = document.querySelector(".languages");
+    if (imgBlock && infoBlock && langBlock) {
+        imgBlock.style.height = "";
+        infoBlock.style.height = "";
+        langBlock.style.height = "";
+        const imgHeight = imgBlock.offsetHeight;
+        const infoHeight = infoBlock.offsetHeight;
+        const langHeight = langBlock.offsetHeight;
+        const maxHeight = Math.max(imgHeight, infoHeight, langHeight);
+        imgBlock.style.height = maxHeight + "px";
+        infoBlock.style.height = maxHeight + "px";
+        langBlock.style.height = maxHeight + "px";
+        const img = imgBlock.querySelector("img");
+        if (img) {
+            img.style.height = maxHeight - 2 * 32 + "px";
+            img.style.width = img.style.height;
+            img.style.objectFit = "cover";
+        }
+    }
+}
+
+function syncProfileImageSquare() {
+    const imgBlock = document.querySelector(".profile-image img");
+    if (imgBlock) {
+        const height = imgBlock.offsetHeight;
+        imgBlock.style.width = height + "px";
+        imgBlock.style.objectFit = "cover";
+    }
+}
+
 function onResizeOrLoad() {
     adjustContactCardMargin();
     syncToolsExperienceHeight();
+    syncProfileBlocksHeight();
+    syncProfileImageSquare();
 }
 
 window.addEventListener("DOMContentLoaded", onResizeOrLoad);
